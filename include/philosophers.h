64af0b7ebcb6129a2d4f2f8612f0390ade966e07
migmanu <jmanuelmigoya@gmail.com>
2023-10-27 19:04:48 +0200

    Add first protos

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,8 +6,32 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 18:22:44 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/27 18:22:46 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/27 18:46:01 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <pthread.h>
+# include <string.h>
 
+# define ERROR -1
+# define FAILURE 1
+# define SUCCESS 0
+
+/****************	00_check_args	****************/
+// check_args.c
+int	check_args(int argc, char *argv[]);
+/****************	10_utils	****************/
+// ft_atoi.c
+int	ft_atoi(const char *str);
+#endif
