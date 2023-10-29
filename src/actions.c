68d0898539d760ec5cfe8664b8c6994bf5af534b
migmanu <jmanuelmigoya@gmail.com>
2023-10-29 19:18:59 +0100

    CHange file name

--- src/actions.c
+++ src/actions.c
@@ -6,9 +6,19 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 16:36:27 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/29 16:36:45 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/29 19:08:48 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philosophers.h"
 
+void	think(int	id)
+{
+	printf("Philosopher %d is thinking\n", id);
+}
+
+void	p_sleep(t_data data, int id)
+{
+	printf("Philosopher %d is gonna mimir\n", id);
+	usleep(data.sleep_time);
+}
