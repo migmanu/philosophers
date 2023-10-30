5041f2165a22f68b1d48a7b75c3ddda1a72f0f61
migmanu <jmanuelmigoya@gmail.com>
2023-10-30 21:18:48 +0100

    Create hold_forks function

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 16:36:27 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/30 21:13:23 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/30 21:18:13 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,31 @@ void	p_sleep(t_philos philo)
 	ft_usleep(philo.sleep_time);
 }
 
-void	eat(t_philos *philo)
+void	hold_forks(t_philos *philo)
 {
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->l_fork);
 		printf("%lu | philo nbr %d took their left fork\n",
-			get_time(), philo->id);
+				get_time(), philo->id);
 		pthread_mutex_lock(philo->r_fork);
 		printf("%lu | philo nbr %d took their right fork\n",
-			get_time(), philo->id);
+				get_time(), philo->id);
 	}
 	else
 	{
 		pthread_mutex_lock(philo->r_fork);
 		printf("%lu | philo nbr %d took their right fork\n",
-			get_time(), philo->id);
+				get_time(), philo->id);
 		pthread_mutex_lock(philo->l_fork);
 		printf("%lu | philo nbr %d took their left fork\n",
-			get_time(), philo->id);
+				get_time(), philo->id);
 	}
+}
+
+void	eat(t_philos *philo)
+{
+	hold_forks(philo);
 	pthread_mutex_lock(philo->eating);
 	printf("%lu | philo nbr %d is gonna eat\n", get_time(), philo->id);
 	ft_usleep(philo->eat_time);
