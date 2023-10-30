3627baf1b4c5cc04eca3a72b71a0d0a1e9db47fe
migmanu <jmanuelmigoya@gmail.com>
2023-10-30 20:46:44 +0100

    Change usleep for ft_usleep

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 16:36:27 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/30 18:32:10 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/30 20:44:47 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,37 @@
 
 void	think(t_philos philo)
 {
-	printf("Philosopher %d is thinking\n\n\n", philo.id); // TODO: erase newline
+	printf("Philosopher %d is thinking\n", philo.id);
 }
 
 void	p_sleep(t_philos philo)
 {
 	printf("Philosopher %d is gonna mimir for\n", philo.id);
-	usleep(philo.sleep_time);
+	ft_usleep(philo.sleep_time);
 }
 
-void	eat(t_philos philo)
+void	eat(t_philos *philo)
 {
-	printf("philo nbr %d is eating\n", philo.id);
-	usleep(philo.eat_time);
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(philo->l_fork);
+		printf("philo nbr %d locked their left fork\n", philo->id);
+		pthread_mutex_lock(philo->r_fork);
+		printf("philo nbr %d locked their right fork\n", philo->id);
+	}
+	else
+	{
+		pthread_mutex_lock(philo->r_fork);
+		printf("philo nbr %d locked their right fork\n", philo->id);
+		pthread_mutex_lock(philo->l_fork);
+		printf("philo nbr %d locked their left fork\n", philo->id);
+	}
+	pthread_mutex_lock(philo->eating);
+	printf("philo nbr %d is gonna eat\n", philo->id);
+	ft_usleep(philo->eat_time);
+	pthread_mutex_unlock(philo->eating);
+	pthread_mutex_unlock(philo->l_fork);
+	printf("philo nbr %d unlocked their left fork\n", philo->id);
+	pthread_mutex_unlock(philo->r_fork);
+	printf("philo nbr %d unlocked their right fork\n", philo->id);
 }
--- src/start_threads.c
+++ src/start_threads.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 19:09:39 by migmanu           #+#    #+#             */
-/*   Updated: 2023/10/30 18:37:43 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/10/30 20:44:25 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,16 @@ void	*philo_routine(void *ptr)
 {
 	t_philos *philo;
 
-	// TODO: check philo life
 	philo = (t_philos *)ptr;
 	if (philo->id % 2 == 0)
-		usleep(100); // TODO: check!
+		ft_usleep(10); // TODO: check!
 	while (*(philo->dead) != 1)
 	{
-		pthread_mutex_lock(philo->l_fork);
-		printf("philo nbr %d locked their left fork\n", philo->id);
-		pthread_mutex_lock(philo->r_fork);
-		printf("philo nbr %d locked their right fork\n", philo->id);
-		// TODO: lock eating
-		eat(*philo);
-		pthread_mutex_unlock(philo->l_fork);
-		printf("philo nbr %d unlocked their left fork\n", philo->id);
-		pthread_mutex_unlock(philo->r_fork);
-		printf("philo nbr %d unlocked their right fork\n", philo->id);
+		// TODO: check philo life
+		eat(philo);
 		p_sleep(*philo);
 		think(*philo);
 	}
-	printf("while end!\n");
 	return (ptr);
 }
 
